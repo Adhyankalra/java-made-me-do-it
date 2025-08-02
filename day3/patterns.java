a4eaf04d529db57a2fd22037eb944a5942f519c5
Adhyankalra <adhyankalraa@gmail.com>
2025-08-02 11:44:12 +0530

    added day3

---
+++ day3/patterns.java
@@ -0,0 +1,24 @@
+public class patterns{
+    public static void main(String[] args) {
+        int n = 9; // Size of the pattern
+        printPattern(n);
+    }
+
+    public static void printPattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");     
+            }
+    }
+
+
+}
+    
+}
+
